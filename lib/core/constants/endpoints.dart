5d2bf5c89c401b6370c43abc095d70067faa189b
Ephraimraxy <124668425+Ephraimraxy@users.noreply.github.com>
2026-01-30 09:08:32 +0100

    fix: use correct 'library/rust' namespace for official images

--- lib/core/constants/endpoints.dart
+++ lib/core/constants/endpoints.dart
@@ -1,5 +1,9 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 class Endpoints {
-  static const String webDomain = 'https://bbmeet.site';
-  static const String baseUrl = 'https://api.bbmeet.site';
+  static String get webDomain =>
+      dotenv.get('WEB_DOMAIN', fallback: 'https://bbmeet.site');
+  static String get baseUrl =>
+      dotenv.get('API_URL', fallback: 'https://api.bbmeet.site');
   static const String suffixUrl = '/';
 }
